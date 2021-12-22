8b540d472412248df2350972dfea65524cda1214
Florin Lazar <florin.lazar.it2014@gmail.com>
2021-12-22 21:33:34 +0200

    Sample reading and printing an array

---
+++ ArrayExercises/ArrayExercises/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ArrayExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Citire array
+            int[] array = ReadArray("Array");
+
+            PrintArray("Array", array);
+        }
+
+        static int ReadNumber(string label, int defaultValue)
+        {
+            Console.Write(label);
+            string value = Console.ReadLine();
+            if (int.TryParse(value, out int result))
+            {
+                return result;
+            }
+
+            return defaultValue;
+        }
+
+        static int[] ReadArray(string label)
+        {
+            Console.WriteLine(label);
+            int nr = ReadNumber("Elements Count=", 0);
+            int[] array = new int[nr];
+            for (int i = 0; i < array.Length; i++)
+            {
+                int element = ReadNumber($"Element[{i}]=", 0);
+                array[i] = element;
+            }
+
+            return array;
+        }
+
+        static void PrintArray(string label, int[] array)
+        {
+            Console.WriteLine();
+            string elementsList = string.Join(", ", array);
+            Console.WriteLine($"{label}={elementsList}");
+        }
+    }
+}
