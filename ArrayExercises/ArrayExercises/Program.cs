530e8595c59ff06f7644707b1a5b91451c916d7e
Florin Lazar <florin.lazar.it2014@gmail.com>
2022-01-12 19:06:44 +0200

    Implement min calculation

--- ArrayExercises/ArrayExercises/Program.cs
+++ ArrayExercises/ArrayExercises/Program.cs
@@ -10,6 +10,17 @@ namespace ArrayExercises
             int[] array = ReadArray("Array");
 
             PrintArray("Array", array);
+
+            // calcul minim
+            int min = Min(array);
+            if (min == int.MinValue)
+            {
+                Console.WriteLine("Min cannot be calculated!");
+            }
+            else
+            {
+                Console.WriteLine($"Min={min}");
+            }
         }
 
         static int ReadNumber(string label, int defaultValue)
@@ -43,6 +54,36 @@ namespace ArrayExercises
             Console.WriteLine();
             string elementsList = string.Join(", ", array);
             Console.WriteLine($"{label}={elementsList}");
+        }
+
+        /// <summary>
+        /// Returns the minimum element from an array, 
+        /// or int.MinValue if minimum cannot be calculated.
+        /// </summary>
+        /// <param name="array">The array.</param>
+        /// <returns>
+        /// The minimum element from an array, 
+        /// or int.MinValue if minimum cannot be calculated.
+        /// </returns>
+        static int Min(int[] array)
+        {
+            if (array is null || array.Length == 0)
+            {
+                // nu pot calcula minim
+                return int.MinValue;
+            }
+
+            // pot calcula minim
+            int min = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] < min)
+                {
+                    min = array[i];
+                }
+            }
+
+            return min;
         }
     }
 }
