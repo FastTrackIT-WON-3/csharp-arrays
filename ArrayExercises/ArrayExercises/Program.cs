8db36daa1e5ef8bf876761357c82b4056269e179
Florin Lazar <florin.lazar.it2014@gmail.com>
2022-01-12 19:17:24 +0200

    Implement max and index-of

--- ArrayExercises/ArrayExercises/Program.cs
+++ ArrayExercises/ArrayExercises/Program.cs
@@ -21,6 +21,28 @@ namespace ArrayExercises
             {
                 Console.WriteLine($"Min={min}");
             }
+
+            // calcul maxim
+            int max = Max(array);
+            if (min == int.MaxValue)
+            {
+                Console.WriteLine("Max cannot be calculated!");
+            }
+            else
+            {
+                Console.WriteLine($"Max={max}");
+            }
+
+            int element = ReadNumber("Element=", 0);
+            int indexOfElement = IndexOf(array, element);
+            if (indexOfElement == -1)
+            {
+                Console.WriteLine($"Element {element} was not found between the elements of the array");
+            }
+            else
+            {
+                Console.WriteLine($"Index of {element} = {indexOfElement}");
+            }
         }
 
         static int ReadNumber(string label, int defaultValue)
@@ -84,6 +106,62 @@ namespace ArrayExercises
             }
 
             return min;
+        }
+
+        /// <summary>
+        /// Returns the maximum element from an array, 
+        /// or int.MaxValue if maximum cannot be calculated.
+        /// </summary>
+        /// <param name="array">The array.</param>
+        /// <returns>
+        /// The maximum element from an array, 
+        /// or int.MaxValue if maximum cannot be calculated.
+        /// </returns>
+        static int Max(int[] array)
+        {
+            if (array is null || array.Length == 0)
+            {
+                // nu pot calcula maxim
+                return int.MaxValue;
+            }
+
+            // pot calcula maxim
+            int max = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] > max)
+                {
+                    max = array[i];
+                }
+            }
+
+            return max;
+        }
+
+        /// <summary>
+        /// Returns the index of the specified element.
+        /// </summary>
+        /// <param name="array">The array.</param>
+        /// <param name="element">The lookup element.</param>
+        /// <returns>The zero based index of the specified element if found, -1 otherwise.</returns>
+        static int IndexOf(int[] array, int element)
+        {
+            if (array is null || array.Length == 0)
+            {
+                return -1;
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                // element was found at index "i"
+                if (array[i] == element)
+                {
+                    return i;
+                }
+            }
+
+            // the element was not found in the array
+            return -1;
         }
     }
 }
